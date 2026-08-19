5e7bc029d14f6e06569a0e75887fe6112ec86147
Fábio de Moraes Ribeiro <fmribeiro.info@gmail.com>
2026-08-19 11:38:20 -0300

    feat: adiciona teste E2E de login com sucesso no Swag Labs

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,24 @@
 const { defineConfig } = require("cypress");
+const createBundler = require("@bahmutov/cypress-esbuild-preprocessor");
+const preprocessor = require("@badeball/cypress-cucumber-preprocessor");
+const createEsbuildPlugin = require("@badeball/cypress-cucumber-preprocessor/esbuild").createEsbuildPlugin;
 
 module.exports = defineConfig({
-  allowCypressEnv: false,
-
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
+    specPattern: "**/*.feature",
+    async setupNodeEvents(on, config) {
+      // 1. Aciona o tradutor Badeball
+      await preprocessor.addCucumberPreprocessorPlugin(on, config);
+
+      // 2. Troca o motor padrão pelo Esbuild e intercepta os arquivos .feature
+      on(
+        "file:preprocessor",
+        createBundler({
+          plugins: [createEsbuildPlugin(config)],
+        })
+      );
+
+      return config;
     },
   },
 });
---
+++ cypress/e2e/login.feature
@@ -0,0 +1,9 @@
+# language: pt
+Funcionalidade: Login do sistema
+
+Cenário: Login com sucesso
+Dado que estou na tela de login
+Quando preencho o campo e-mail com um e-mail válido
+E preencho o campo senha com uma senha válida
+E clico em Acessar
+Então o título de Produtos deve aparecer na tela
