00f600d956a2ddbad2a93cd6165acdb70ea0debf
Fábio de Moraes Ribeiro <fmribeiro.info@gmail.com>
2026-08-27 02:40:38 -0300

    feat: criacao de mocku, interceptacao de rede para validacao de dados

--- cypress/e2e/common/shared_steps.js
+++ cypress/e2e/common/shared_steps.js
@@ -8,6 +8,14 @@ Given("que estou na tela de login", () => {
   cy.visit("https://www.saucedemo.com/");
 });
 
+Given("que estou na tela de testes de rede do Cypress", () => {
+  cy.visit('https://example.cypress.io/commands/network-requests');
+});
+
+Given("intercepto a rota de comentarios", () => {
+  cy.intercept('GET', '**/comments/*', { fixture: 'comentario_hackeado.json'}).as('chamadaRede');
+});
+
 When("preencho o campo {string} com {string}", (nomeDoCampo, valorDoTexto) => {
     if(valorDoTexto !== ""){
       cy.get(`[data-test="${nomeDoCampo}"]`).type(valorDoTexto);
@@ -18,10 +26,19 @@ When("clico no botão {string}", (nomeDoBotao) => {
   cy.get(`[data-test="${nomeDoBotao}"]`).click();
 });
 
+When("clico no botão de buscar dados", () => {
+  cy.get('.network-btn').click();
+});
+
 Then("devo ver a mensagem de sucesso {string}", (mensagem) => {
   cy.contains(mensagem).should('be.visible');
 });
 
 Then("devo ver a mensagem de erro {string}", (mensagem) => {
   cy.contains(mensagem).should('be.visible');
+});
+
+Then("a tela deve exibir a minha carga hackeada", () => {
+  cy.wait('@chamadaRede');
+  cy.contains('Sucesso absoluto!').should('be.visible');
 });
